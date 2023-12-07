b9592739056d5c4d91a9f378049bed5738d22c27
Florent <bwb.f.de-crescenzo@fondespierre.com>
2023-12-07 14:15:35 +0100

    EV-23 <Structure importe module frameword>

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const cors = require("cors");
+const express = require("express")
+const app = express();
+const connection = require()
+const bodyParser = require()
+const routesProducts = require("")
+const usersRoutes = require()
+require("dotenv").config();
+
